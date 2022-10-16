ffbc342662edf319235dd043ef4a76f1d27a174a
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2022-10-16 16:02:19 +0200

    Create the plan for code 'work to do'

---
+++ tic-tac-toe.py
@@ -0,0 +1,15 @@
+# ---------------
+# WORK TO DO:
+#
+# 1. Function with active game
+# 2. Create empty board
+# 3. Drawing board (checking if it's empty or not)
+# 4. Change player
+# 5. Result of the game:
+#     a) tie
+#     b) win:
+#         - diagonals
+#         - horizontal lines
+#         - vertical lines
+
+
