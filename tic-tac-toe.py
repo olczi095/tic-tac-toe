a1fb2ce9975aeee885d9560e6babfa86b347427d
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2022-10-17 12:47:38 +0200

    Add and modify some code
    Remove the function 'printing_board', change the function 'check_the_winner' into 'if_someone_win' and add a function 'game'. Do some other staff in order to the game working properly (still not everything)

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -19,48 +19,81 @@ def the_game():
 
     board = [['_' for x in range(3)] for x in range(3)]
 
-    def printing_board():
-        return board
 
-
-    def diagonal_win(fill_board):
+    def diagonal_win(game_board):
         #Check if someone won on the diagonal lines.
-        return fill_board[1][1] == fill_board[0][0] ==  fill_board[2][2] or fill_board[0][2] == fill_board[1][1] == fill_board[2][0]
+        if game_board[1][1] == game_board[0][0] ==  game_board[2][2] or game_board[0][2] == game_board[1][1] == game_board[2][0]:
+            if game_board[1][1] != '_':
+                return game_board[1][1]
+        return False
 
 
-    def horizontal_win(fill_board):
+    def horizontal_win(game_board):
         #Check if someone won on the horizontal lines.
         for x in range(3):
-            if len(set(fill_board[x])) == 1:
-                return fill_board[x][0]
+            if len(set(game_board[x])) == 1:
+                if game_board[x][0] != '_':
+                    return game_board[x][0]
         return False
 
 
-    def vertical_win(fill_board):
+    def vertical_win(game_board):
         #Check if someone won on the diagonals.
         for x in range(3):
-            if fill_board[0][x] == fill_board[1][x] == fill_board[2][x]:
-                return True
+            if game_board[0][x] == game_board[1][x] == game_board[2][x]:
+                if game_board[0][x] != '_':
+                    return game_board[0][x]
         return False
 
 
-    def check_the_winner(fill_board):
-        if not horizontal_win(fill_board):
-            if
+    def if_someone_win(game_board):
+        #Check if someone already won or not.
+        if horizontal_win(game_board):
+            return horizontal_win(game_board)
 
-        elif diagonal_win(fill_board):
+        elif diagonal_win(game_board):
+            return diagonal_win(game_board)
 
-        elif vertical_win(fill_board):
+        elif vertical_win(game_board):
+            return vertical_win(game_board)
+
+
+    def game(board, player, position):
+        position = position - 1
+        row = position // 3
+        column = position % 3
+        if board[row][column] == '_':
+            if player == player1:
+                board[row][column] = 'X'
+            else:
+                board[row][column] = 'O'
+        else:
+            return False
 
 
     print("\nHello players!\nThis is a tic-tac-toe game. Let's start!\n" + "-" * 30 + "\n\n")
 
-    # player1 = input("Player 1 type your name:\n>>> ")
-    hello <p1> you will have an sign.
-    # player2 = input("Player 2 type your name:\n>>> ")
-    print("\n\n")
+    player1 = input("Player 1 type your name:\n>>> ")
+    player2 = input("Player 2 type your name:\n>>> ")
+    print(f"\nHello players! {player1} has a sign 'X' and {player2} has a sign 'O'. You can choose the location from 1 to 9. "
+          f"{player1} you start!\n")
+    print(board)
+    print("\n")
 
+    game_continue = True
 
+    while game_continue:
+        move_p1 = int(input(f"{player1} type your number: "))
+        if game(board, player1, move_p1) is False:
+            break
+        else:
+            print(board)
+            if not if_someone_win(board):
+                move_p2 = int(input(f"{player2} type your number: "))
+                if game(board, player2, move_p2) is False:
+                    break
+                else:
+                    print(board)
 
 
 the_game()
