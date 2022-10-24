154fdc8ee81916a40218d2e26ce8b92355ea0875
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2022-10-24 10:50:25 +0200

    Modify the code to make it work properly

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -13,14 +13,15 @@
 #         - vertical lines
 
 
-board = [['_' for x in range(3)] for y in range(3)]
-
-
 def the_game():
     """The 'tic-tac-toe' game for two players.
     Both players move successively until one of them wins or the game ends with a tie."""
 
-    global board
+    board = [['_' for _ in range(3)] for _ in range(3)]
+
+
+    def printing_board(board):
+        return
 
     def diagonal_win(game_board):
         # Check if someone won on the diagonal lines.
@@ -58,14 +59,20 @@ def the_game():
             return vertical_win(game_board)
 
     def game(g_board, player, position):
-        position = position - 1
-        row = position // 3
-        column = position % 3
-        if g_board[row][column] == '_':
-            if player == player1:
-                g_board[row][column] = 'X'
-            else:
-                g_board[row][column] = 'O'
+        # Function with the right game.
+        if position in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
+            # Check if the user input is correct (number from 1 to 9)
+            position = int(position)
+            position = position - 1
+            row = position // 3
+            column = position % 3
+
+            if g_board[row][column] == '_':
+                if player == player1:
+                    g_board[row][column] = 'X'
+                else:
+                    g_board[row][column] = 'O'
+
         else:
             return False
 
@@ -73,10 +80,12 @@ def the_game():
 
     player1 = input("Player 1 type your name:\n>>> ")
     player2 = input("Player 2 type your name:\n>>> ")
+
     print(
         f"\nHello players! {player1} has a sign 'X' and {player2} has a sign 'O'. "
         f"You can choose the location from 1 to 9. "
         f"{player1} you start!\n")
+
     print(board)
     print("\n")
 
@@ -85,26 +94,37 @@ def the_game():
     while game_continue:
 
         if if_someone_win(board):
-            print("PLAYER 2 WON!!!")
+            print(f"{player2} won!!!")
             break
 
         else:
 
-            move_p1 = int(input(f"{player1} type your number: "))
-            if game(board, player1, move_p1) is False:
-                # Check if the place chosen by the player1 is already filled.
-                break
+            move_p1 = input(f"{player1} type your number: ")
+
+            while game(board, player1, move_p1) is False:
+                # Check if the place chosen by the player1 is already filled or the input is wrong.
+                print("Your choice is wrong. Try again! ")
+                print(f"{board}\n")
+                move_p1 = input(f"{player1} type your number: ")
+
             else:
-                print(board)
+
+                print(f"{board}\n")
                 if not if_someone_win(board):
-                    move_p2 = int(input(f"{player2} type your number: "))
-                    if game(board, player2, move_p2) is False:
-                        # Check if the place chosen by the player2 is already filled.
-                        break
+                    move_p2 = input(f"{player2} type your number: ")
+
+                    while game(board, player2, move_p2) is False:
+                        # Check if the place chosen by the player2 is already filled or the input is wrong.
+                        print("Your choice is wrong. Try again! ")
+                        print(f"{board}\n")
+                        move_p2 = input(f"{player2} type your number: ")
+
                     else:
-                        print(board)
+
+                        print(f"{board}\n")
                 else:
-                    print("PLAYER 1 WON!!!")
+
+                    print(f"{player1} won!!!")
                     game_continue = False
 
 
